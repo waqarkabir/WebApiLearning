fadb71e113d6906b9b427bd4b4b895b41648ff9b
waqar <waqarkabir10@gmail.com>
2022-08-14 10:29:11 +0500

    Added Employee API Controller

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace WebApiLearning.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        public EmployeeController()
+        {
+
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        { 
+            return new List<string>() {"Waqar Kabir"};
+        }
+    }
+}
